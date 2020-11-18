ac6ef208dc05ab02918fad1124b4a990f93ff12a
adrian154 <94568a@gmail.com>
2020-11-18 09:01:58 -0800

    inti

---
+++ src/main/java/dev/codesoup/mc/CustomMod.java
@@ -0,0 +1,32 @@
+package dev.codesoup.mc;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = CustomMod.MODID, name = CustomMod.NAME, version = CustomMod.VERSION)
+public class CustomMod
+{
+    public static final String MODID = "custommod";
+    public static final String NAME = "Custom Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
